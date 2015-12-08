dce202615513b462df908d639ad9ae9d12e1b444
mayormcmatt <matthew.is.riding@gmail.com>
2015-12-07 22:51:30 -0800

    mongoDB dependency issues, reset and get moving again

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-var db = require('./model/db');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
